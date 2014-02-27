fe51086ca970a7e297b8d254a7fe2e8b38487d7b
Grenmyr <dg222cs@student.lnu.se>
2014-02-27 10:50:58 +0100

    Fortsätter snart.

--- Äventyrliga kontakter/Äventyrliga kontakter/Model/Contact.cs
+++ Äventyrliga kontakter/Äventyrliga kontakter/Model/Contact.cs
@@ -8,12 +8,17 @@ namespace Äventyrliga_kontakter.Model
 {
     public class Contact
     {
-        
         public int ContactId { get; set; }
-        [Required(ErrorMessage="Fält får ej lämnas tomt.")]
+        [Required(ErrorMessage="Fält får ej lämnas tomt.")]    
+        [StringLength(50, ErrorMessage = "Email kan max vara 50 tecken långt.")]
+        [EmailAddress(ErrorMessage = "Det går ej tolka som giltig Email, Försök igen.")]
         public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Fält får ej lämnas tomt.")]
+        [StringLength(50, ErrorMessage = "Förnamn kan max vara 50 tecken långt.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Fält får ej lämnas tomt.")]
+        [StringLength(50, ErrorMessage = "EfterNamn kan max vara 50 tecken långt.")]
+        public string LastName { get; set; }
     }
 }
--- Äventyrliga kontakter/Äventyrliga kontakter/default.aspx.cs
+++ Äventyrliga kontakter/Äventyrliga kontakter/default.aspx.cs
@@ -11,41 +11,37 @@ namespace Äventyrliga_kontakter
     public partial class _default : System.Web.UI.Page
     {
         private Service _service;
-        
+
 
         // Egenskap för skapa Service referens endast om det behövs.
         private Service Service
         {
             get { return _service ?? (_service = new Service()); }
         }
-
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
         // Genererar alla kontakter.
         public IEnumerable<Äventyrliga_kontakter.Model.Contact> ContactListView_GetData(int maximumRows, int startRowIndex, out int totalRowCount)
         {
-            
             //var fullContactList = Service.GetContacts();
-            
             return Service.GetContactsPageWise(maximumRows, startRowIndex, out totalRowCount).OrderBy(c => c.FirstName);
         }
 
         public void ContactListView_InsertItem(Contact contact)
         {
-            // Metod för lägga in kontakter till min databas.
-
-            // TODO: Ska lägga in isvalid här sen.
-
-            try
+            // Metod för lägga in kontakter till min databas. FATTTAR INTE MODELSTATE
+            if (ModelState.IsValid)
             {
-                Service.SaveContact(contact);
-            }
-            catch (Exception)
-            {
-                ModelState.AddModelError(String.Empty, "Fel inträffade när Kunduppgift skulle Läggas till.");
+                try
+                {
+                    Service.SaveContact(contact);
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError(String.Empty, "Fel inträffade när Kunduppgift skulle Läggas till.");
+                }
             }
 
         }
@@ -53,32 +49,29 @@ namespace Äventyrliga_kontakter
         // The id parameter name should match the DataKeyNames value set on the control
         public void ContactListView_UpdateItem(int contactId)
         {
-            try
+            if (ModelState.IsValid)
             {
-                var contact = Service.GetContact(contactId);
-                if (Service.GetContact(contactId) == null)
+                try
                 {
-                    //varför är string emty bättre.
-                    ModelState.AddModelError(String.Empty, "Fel inträffade när kontaktuppgift skulle Sparas.");
-                    return;
+                    var contact = Service.GetContact(contactId);
+                    if (Service.GetContact(contactId) == null)
+                    {
+                        ModelState.AddModelError(String.Empty, "Fel inträffade när kontaktuppgift skulle Sparas.");
+                        return;
+                    }
+                    // TODO: Ska lägga in isvalid här sen.
+
+                    // Chanser, tryupdatemodel Validerar objktet Contact, eftersom vi hämtar det från server.
+                    if (TryUpdateModel(contact))
+                    {
+                        Service.SaveContact(contact);
+                    }
                 }
-
-                // TODO: Ska lägga in isvalid här sen.
-
-                // Chanser, tryupdatemodel försöker spara min kontakt i min tabell. om true så sparar jag även i mitt affärslager.
-                if (TryUpdateModel(contact))
+                catch (Exception)
                 {
-
-                    Service.SaveContact(contact);
+                    ModelState.AddModelError(String.Empty, "Fel inträffade när Kunduppgift skulle Sparas.");
                 }
-
-
             }
-            catch (Exception)
-            {
-                ModelState.AddModelError(String.Empty, "Fel inträffade när Kunduppgift skulle Sparas.");
-            }
-
         }
 
         // The id parameter name should match the DataKeyNames value set on the control
