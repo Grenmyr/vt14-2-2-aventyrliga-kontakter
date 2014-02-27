6e75ec09d642aaa860627539eb53608085492eaa
Grenmyr <dg222cs@student.lnu.se>
2014-02-27 09:55:55 +0100

    Jobbar med validetingen, och lagt in Jquery

---
+++ Äventyrliga kontakter/Äventyrliga kontakter/App_Infrastructure/ValidationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Äventyrliga_kontakter
+{
+    public static class ValidationExtensions
+    {
+        // Works aproximatly, This is extension method to my Validation on properties in Contact.cs.
+        // They are used to initialize 1 ValidationContext that if if no Failed ValidationResults returns true = no failed property validations.
+        public static bool Validate<Typ>(this Typ instance, out ICollection<ValidationResult> validationResults)
+        {
+            var validationContext = new ValidationContext(instance);
+            validationResults = new List<ValidationResult>();
+            return Validator.TryValidateObject(instance,validationContext,validationResults,true);
+        }
+    }
+}
--- Äventyrliga kontakter/Äventyrliga kontakter/Model/Contact.cs
+++ Äventyrliga kontakter/Äventyrliga kontakter/Model/Contact.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Äventyrliga_kontakter.Model
 {
     public class Contact
     {
+        
         public int ContactId { get; set; }
+        [Required(ErrorMessage="Fält får ej lämnas tomt.")]
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
